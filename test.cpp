0018c5a70a2ddbf90f302bf8d330a3b6f33e3ad8
Abdrakhman Safin <asafin2012@litsey2.ru>
2017-01-16 16:55:02 +0300

    some commit

--- test.cpp
+++ test.cpp
@@ -7,14 +7,7 @@
 #include <cstdio>
 using namespace std;
 
-Segment_tree *t;
-
 int main(int argc, char **argv){
-	int arr[] = {1, 2, 3, 4, 5, 6, 7};
-
-	t = new Segment_tree(arr, 7);
-
-	cout << t->get(2, 2) << endl;
-
+	
 	return 0;
 }
